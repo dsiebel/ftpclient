6f49740ea10bc30bc4063f19b1c84a78fa4cd077
Dominik Siebel <dsiebel@suckbook.(none)>
2010-04-03 17:53:14 +0200

    * switched config to magic __get and __call (read-only)

--- lib/Ftp/Client/Config.php
+++ lib/Ftp/Client/Config.php
@@ -6,22 +6,31 @@ class Ftp_Client_Config
 
 	const MODE_ASCII = FTP_ASCII;
 
-	public $port;
+	protected $aParams = array();
 
-	public $timeout;
-
-	public $pasv;
-
-	public $ssl;
-
-	public $mode;
-
-	public function __construct()
+	public function __construct(array $aParams = array())
 	{
-		$this->port = 21;
-		$this->timeout = 90;
-		$this->pasv = false;
-		$this->ssl = false;
-		$this->mode = self::MODE_BINARY;
-	}
+		if (empty($aParams['port'])) $aParams['port'] = 21;
+		if (empty($aParams['timeout'])) $aParams['timeout'] = 90;
+		if (empty($aParams['pasv'])) $aParams['pasv'] = false;
+		if (empty($aParams['ssl'])) $aParams['ssl'] = false;
+		if (empty($aParams['transfermode'])) $aParams['transfermode'] = self::MODE_BINARY;
+
+		$this->aParams = $aParams;
+	} // function
+
+	public function __call($sMethodname, array $aParams = array())
+	{
+		if (substr($sMethodname, 0, 3) == 'get' && empty($aParams))
+		{
+			if (!empty($this->aParams[strtolower(substr($sMethodname, 3))]))
+				return $this->aParams[strtolower(substr($sMethodname, 3))];
+		} // if
+	} // function
+
+	public function __get($sPropertyname)
+	{
+		if (!empty($this->aParams[strtolower($sPropertyname)]))
+			return $this->aParams[strtolower($sPropertyname)];
+	} // function
 } // class
